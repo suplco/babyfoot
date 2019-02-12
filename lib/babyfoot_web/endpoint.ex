ebffbd81bdafd2edf06b94be60c1215ebd0c4e17
Thomas Kliszowski <thomas.kliszowski@gmail.com>
2019-02-12 15:18:38 +0100

    use heroku as cloud provider

--- lib/babyfoot_web/endpoint.ex
+++ lib/babyfoot_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule BabyfootWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :babyfoot
 
   socket "/socket", BabyfootWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
